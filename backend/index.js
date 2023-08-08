78e4237e3bc9f34db972716cebf183f86c072ebe
Anil sharma <smartanil0000@gmail.com>
2023-08-08 09:58:44 +0530

    Api Commit

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config();
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log("DB Connected Successfully!"))
+.catch((err)=>{
+    console.log(err)
+})
+
+app.use(express.json())
+
+//middleware
+app.use("/api/auth",authRoute)
+app.use("/api/products",productRoute)
+app.use("/api/user",userRoute)
+app.use("/api/carts",cartRoute)
+app.use("/api/orders",orderRoute)
+
+
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("Backend server is running!")
+})
